a27cb3b553344b69275870c7a09a377ba024c871
abarikdar <73364852+abarikdar@users.noreply.github.com>
2023-11-09 13:24:31 -0500

    added cat breed endpoint (#4)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import { useState } from 'react';
 
 export default function App() {
   const [catImages, setCatImages] = useState([]);
+  const [breed, setBreed] = useState('ex. beng ( only include first 4 letters )');
+
+  function handleBreedChange(e) {
+    setBreed(e.target.value);
+  }
 
   function getCatImages() {
     fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+      .then((response) => response.json())
+      .then((data) => setCatImages(data));
+  }
+  
+  function getCatBreedImages() {
+    fetch('https://api.thecatapi.com/v1/images/search?limit=1&breed_ids='+breed)
       .then((response) => response.json())
       .then((data) => setCatImages(data));
   }
@@ -12,6 +23,17 @@ export default function App() {
   return (
     <main className="container">
       <button onClick={getCatImages}>Get Cat Images</button>
+      <label>
+        Find Picture of Specific Cat Breeds:
+        <input onChange={handleBreedChange} value={breed} />
+      </label>
+      <button onClick={getCatBreedImages}>Get Image</button>
+      {catImages && (
+        <figure>
+          <figcaption>{catImages.name}</figcaption>
+        </figure>
+      )}
+
       <div className="image-grid">
         {catImages.map((catImage) => (
           <img
