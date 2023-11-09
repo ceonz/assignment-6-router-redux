c8a97a7793312c2369fbbbb150789124a75cd69e
ceonz <45135014+ceonz@users.noreply.github.com>
2023-11-09 00:01:12 -0500

    add cat api (#2)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [catImages, setCatImages] = useState([]);
+
+  function getCatImages() {
+    fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+      .then((response) => response.json())
+      .then((data) => setCatImages(data));
+  }
+
   return (
-    <div>Hello World!</div>
+    <main className="container">
+      <button onClick={getCatImages}>Get Cat Images</button>
+      <div className="image-grid">
+        {catImages.map((catImage) => (
+          <img
+            key={catImage.id}
+            src={catImage.url}
+            alt="Random cat"
+            width="300"
+            height="300"
+          />
+        ))}
+      </div>
+    </main>
   );
 }
---
+++ src/apis.js
@@ -0,0 +1,51 @@
+const API_KEY = process.env.REACT_APP_CAT_API_KEY;
+if (!API_KEY) { throw new Error("REACT_APP_CAT_API_KEY not found. Please set it in .env file.") }
+
+const API_URL = "https://api.thecatapi.com/v1";
+
+function request(endpoint, options = {}) {
+  const headers = options.headers || {};
+  return fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    redirect: "follow",
+    headers: Object.assign({
+      "Content-Type": "application/json",
+      "x-api-key": API_KEY,
+    }, headers)
+  })
+    .then(res => res.json())
+}
+
+function getAllImages(limit = 10) {
+  return request(`/images/search?limit=${limit}`)
+}
+
+function getMyUploadedImages() {
+  return request(`/images`)
+}
+
+function uploadImage(file) {
+  const formData = new FormData();
+  formData.append("file", file);
+  return request(`/images/upload`, {
+    method: "POST",
+    body: formData,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    }
+  })
+}
+
+function deleteImage(id) {
+  return request(`/images/${id}`, {
+    method: "DELETE",
+  })
+}
+
+function getVotes() {
+  return request(`/votes`)
+}
+
+function getRandomFacts() {
+  return request(`/facts`)
+}
