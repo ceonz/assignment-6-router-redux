ed41fef07465f5e6622366e5cb8d38a05086128d
ceonz <ceon91@gmail.com>
2023-11-20 21:26:05 -0500

    add routing template

--- src/index.js
+++ src/index.js
@@ -2,10 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './routes/Root';
+import Home from './routes/Home';
+import About from './routes/About';
+import Contact from './routes/Contact';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
