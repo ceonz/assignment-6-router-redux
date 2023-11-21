73d96dcbaca8b0bbb812bfe39e1bee0b657ce488
Jason Chen <73327854+jchen7381@users.noreply.github.com>
2023-11-21 16:57:48 -0500

    Update Vote.js

--- src/routes/Vote.js
+++ src/routes/Vote.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { castVote, setCurrentImageIndex } from '../imagesSlice';
+import { castVote} from '../features/imagesSlice';
 
 
 export default function Vote(){
