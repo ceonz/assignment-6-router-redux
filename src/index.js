e30bf8d6dc0ed56f4738dd2b4964422983d9217f
abarikdar <73364852+abarikdar@users.noreply.github.com>
2023-11-21 03:12:14 -0500

    fix path name and syntax error

--- src/index.js
+++ src/index.js
@@ -38,12 +38,10 @@ const router = createHashRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Provider store={store}>
+    <App />
+    </Provider>
   </React.StrictMode>
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+export default store;
