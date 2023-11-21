9374fbf0a7a61c9af7d87a2a2d22855d505b1c43
ceonz <ceon91@gmail.com>
2023-11-20 21:55:09 -0500

    add cat lists

--- src/routes/List.js
+++ src/routes/List.js
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import { getAllImages } from "../apis";
+
 export default function List() {
+  const [catImages, setCatImages] = useState([]);
+
+  function getCatImages() {
+    getAllImages()
+      .then((data) => {
+        setCatImages(data);
+        //setCurrentImageIndex(0);
+      });
+  }
+
   return (
-    <p>Cat List</p>
+    <div>
+      <button onClick={getCatImages}>Get Cat Images</button>
+
+      <div className="image-grid">
+        {catImages.map((catImage) => {
+          // TODO: Utilizing redux add favorite feature
+          // const isFavorited = favorites.some(fav => fav.image_id === catImage.id);
+          return (
+            <div key={catImage.id} className="cat-card">
+              <img
+                src={catImage.url}
+                alt="Random cat"
+                width="300"
+                height="300"
+              />
+              {/* <button onClick={() => isFavorited ? handleDeleteFavorite(catImage.id) : handleCreateFavorite(catImage.id)}>
+                {isFavorited ? 'Unfavorite' : 'Favorite'}
+              </button> */}
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
